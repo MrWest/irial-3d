6e8cf8497ccc4d035912f792d43ef589f7e1130b
Enrike <kidwest87@gmail.com>
2020-06-24 07:55:58 -0400

    misc: logo Icons

--- pages/_app.js
+++ pages/_app.js
@@ -2081,7 +2081,7 @@ class MyApp extends App {
           ]}
         >
           <meta charset="utf-8" />
-          <link rel="shortcut icon" type="image/x-icon" href="/static/images/logoicon.png" />
+          <link rel="shortcut icon" type="image/x-icon" href="/static/images/public/irial-transparency-logo.png" />
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="robots" content="index, follow" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
